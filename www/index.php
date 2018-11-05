5e39e6b757b96d85b48d41957568318d5c2517aa
webblogme <webblogme@gmail.com>
2018-11-05 21:51:08 +0700

    finish CSS

---
+++ www/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="assets/css/css.bs3.en.css" />
+	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="//www.jsviews.com/download/jsrender.min.js"></script>
+  <script src="assets/js/moment.min.js"></script>
+  <script src="assets/js/livestamp.min.js"></script>
+</head>
+<body>
+	<div class="container">
+
+
+
+
+		<div class="row">
+			<div class="col-xs-12">
+
+
+
+				<div class="outter">
+					<div class="inner">
+						<div>M</div>
+						<div>A</div>
+						<div>Q</div>
+						<div>E</div>
+					</div>
+					<div class="center"></div>
+				</div>
+
+
+		<!-- /.div -->
+	</div>
+	<!-- /.container -->
+</body>
+</html>
