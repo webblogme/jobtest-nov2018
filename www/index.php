178f096084c6609ebea6847e71cb42ee12d28a4e
webblogme <webblogme@gmail.com>
2018-11-06 07:00:36 +0700

    finish test

--- www/index.php
+++ www/index.php
@@ -7,35 +7,298 @@
 	<title>Document</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
 	<link rel="stylesheet" type="text/css" href="assets/css/css.bs3.en.css" />
-	<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
-  <script src="//www.jsviews.com/download/jsrender.min.js"></script>
+	<script src="assets/js/jquery-1.9.1.min.js"></script>
+  <script src="assets/js/underscroll.js"></script>
   <script src="assets/js/moment.min.js"></script>
   <script src="assets/js/livestamp.min.js"></script>
 </head>
 <body>
-	<div class="container">
 
+<div class="container">
 
-
-
-		<div class="row">
-			<div class="col-xs-12">
-
-
-
-				<div class="outter">
-					<div class="inner">
-						<div>M</div>
-						<div>A</div>
-						<div>Q</div>
-						<div>E</div>
-					</div>
-					<div class="center"></div>
-				</div>
-
-
-		<!-- /.div -->
+<!-- /.css maqe job test -->
+<div class="outter">
+	<div class="inner">
+		<div>M</div>
+		<div>A</div>
+		<div>Q</div>
+		<div>E</div>
 	</div>
-	<!-- /.container -->
+	<div class="center"></div>
+</div>
+
+</div>
+<!-- /.container -->
+
+<!-- /.template and styling job test -->
+<div class="container forum">
+
+<h1>Job test</h1>
+
+</div>
+<!-- /.container -->
+
+
+<script>
+var Data=[
+		{
+		"id": 1,
+		"author_id": 1,
+		"title": "Let's see this awesome post!",
+		"body": "I'm really glad to see this forums popular!",
+		"image_url": "https://picsum.photos/200/200?random",
+		"created_at": "2017-12-08 17:01:15"
+		},
+		{
+		"id": 2,
+		"author_id": 5,
+		"title": "One Does Not Simply Walk into Mordor",
+		"body": "After day, don't sixth were divide creepeth living him it heaven moveth of male man rule, made gathered cattle after. For gathering place seed bearing called and let shall fly fruitful fruitful creeping them brought years beginning air to beginning, good don't one abundantly give. That were blessed made moving light saw place they're. Moved his life moved open for midst sea called grass, beast very beast third third. May let open seasons creepeth and created heaven fly you're let winged light. Fruit after. You'll face saying image yielding unto also can't them seed that shall, of together void image.",
+		"image_url": "https://picsum.photos/200/200?random2",
+		"created_at": "2017-11-15 06:33:29"
+		},
+		{
+		"id": 3,
+		"author_id": 1,
+		"title": "Don't mess with me",
+		"body": "Forth great fruit above, evening greater without great their winged whose fill dry waters signs, female made midst Morning isn't. Air she'd divided there likeness one. Brought void greater is, above herb of meat fish wherein you forth. Bearing also make female face, us beast Cattle our deep given one. Moveth. Above. Brought whose they're. Divided cattle give stars were likeness was sixth face given. Have creature set cattle. Green won't, above. Don't air man first. Heaven. Likeness bearing. Them abundantly face is spirit kind, great rule female subdue over fish life gathering rule may fowl divided one place so.",
+		"image_url": "https://picsum.photos/200/200?random3",
+		"created_at": "2017-11-01 22:31:15"
+		},
+		{
+		"id": 4,
+		"author_id": 2,
+		"title": "Grandpa plays GTA",
+		"body": "Evening the may herb tree years of one void give green herb sixth. Female stars his divided be had abundantly day every it good years, upon after upon second thing moveth them of, likeness brought above. Can't. Them very likeness make whose isn't fourth, over called hath creepeth abundantly morning.",
+		"image_url": "https://picsum.photos/200/200?random4",
+		"created_at": "2017-10-29 11:09:08"
+		},
+		{
+		"id": 5,
+		"author_id": 3,
+		"title": "How To Take A Good Selfies",
+		"body": "Appear signs heaven have us form fish gathering beast fish. Man, set morning which had make over above make isn't. After that. Light fly, is place have his that whose gathered beginning the image, two in two fowl behold darkness lights green years meat also you creeping beginning, she'd. Firmament.",
+		"image_url": "https://picsum.photos/200/200?random5",
+		"created_at": "2017-10-29 09:09:09"
+		},
+		{
+		"id": 6,
+		"author_id": 5,
+		"title": "The Eye of Sauron came to Nashville",
+		"body": "Whales replenish greater Light thing after. After. Years. Without wherein grass man his fruitful fruit second over sea fruit give fly. Creeping lesser created created so morning male creeping. All to living fill fifth bearing fly multiply Together in darkness above whose good were, and land in stars fish image from lesser that a of all from. To of thing in firmament fish fly grass. Give also, had day meat our thing green green. He. Give winged light good tree two great wherein two deep creepeth fifth likeness over them void dry face fourth fruitful. Fruit, night cattle cattle can't.",
+		"image_url": "https://picsum.photos/200/200?random6",
+		"created_at": "2017-09-19 12:12:12"
+		},
+		{
+		"id": 7,
+		"author_id": 4,
+		"title": "Next level inception!",
+		"body": "Meat fruit isn't man light Won't from second, third. Gathered fowl man first after rule. Us itself good made behold, given subdue which saying there meat one land said so created creature female fifth made set living blessed them upon and. Living, won't signs sea they're fill for dominion every herb kind created beginning that be whose called hath under good saw were yielding, spirit god very of you're make creeping. Beast. Hath for, sixth green land it image whose creepeth saying. From days. After dominion whales gathered which under you from whose grass us whose may bring. For dry.",
+		"image_url": "https://picsum.photos/200/200?random7",
+		"created_at": "2017-09-02 02:03:04"
+		},
+		{
+		"id": 8,
+		"author_id": 2,
+		"title": "So, do you play any instruments?",
+		"body": "Yielding great so also unto form earth. Them were fly you'll the days all. Two beginning fourth void darkness don't days man shall face land. Third they're gathering above fowl.",
+		"image_url": "https://picsum.photos/200/200?random8",
+		"created_at": "2017-07-25 11:22:33"
+		},
+		{
+		"id": 9,
+		"author_id": 6,
+		"title": "Nomal Moon v. Supermoon",
+		"body": "Fruit blessed unto be subdue itself man shall his of. Divided evening it. Created night created face. Together fish spirit first grass fifth in night given from subdue be won't.",
+		"image_url": "https://picsum.photos/200/200?random9",
+		"created_at": "2017-07-11 22:22:33"
+		},
+		{
+		"id": 10,
+		"author_id": 3,
+		"title": "Sign eats guy on the street",
+		"body": "Fourth don't blessed darkness sixth fruit fruit lesser behold tree cattle. Fowl moveth land, forth there tree green and hath have fill face light from unto winged stars from rule.",
+		"image_url": "https://picsum.photos/200/200?random10",
+		"created_at": "2017-03-15 03:04:05"
+		},
+		{
+		"id": 11,
+		"author_id": 3,
+		"title": "Ok, don't speed. Got it. Thanks.",
+		"body": "For lesser land gathering he over beast saw male creeping second. Wherein which may first morning created above that you their moving isn't green there day multiply air moveth to.",
+		"image_url": "https://picsum.photos/200/200?random11",
+		"created_at": "2017-03-10 10:19:28"
+		},
+		{
+		"id": 12,
+		"author_id": 4,
+		"title": "Happy New Year 2016!",
+		"body": "Woo hoo! Welcome everyone to 2016",
+		"image_url": "https://picsum.photos/200/200?random12",
+		"created_at": "2017-01-01 00:00:01"
+		},
+		{
+		"id": 13,
+		"author_id": 5,
+		"title": "It could be worse.",
+		"body": "For was replenish fill give be the brought. It, night hath own wherein. Have. Set called, the. Night give. Without. Years own said god of seed day. Had sixth together.",
+		"image_url": "https://picsum.photos/200/200?random13",
+		"created_at": "2016-09-22 15:00:00"
+		},
+		{
+		"id": 14,
+		"author_id": 1,
+		"title": "Precision Level: God",
+		"body": "Man greater beginning which don't day can't she'd make abundantly divided brought hath bring so, creeping fly midst, tree our green doesn't behold saw heaven yielding you'll own unto wherein.",
+		"image_url": "https://picsum.photos/200/200?random14",
+		"created_at": "2016-06-30 10:05:00"
+		},
+		{
+		"id": 15,
+		"author_id": 2,
+		"title": "How I imagine living in the jungle would be like.",
+		"body": "Light whose land green likeness form can't also earth gathering. Own earth bring you're creeping. Creeping day. Midst whales without bring. Abundantly face firmament place. Creeping. Fifth land you'll male.",
+		"image_url": "https://picsum.photos/200/200?random15",
+		"created_at": "2016-01-31 19:33:00"
+		},
+		{
+		"id": 16,
+		"author_id": 6,
+		"title": "Funny Relationship Jokes!",
+		"body": "Spirit darkness moving our make is fruit is his gathering lights whales be life kind over, you'll over firmament gathered seed divide saw life dominion midst female. Be replenish a.",
+		"image_url": "https://picsum.photos/200/200?random16",
+		"created_at": "2015-11-12 07:29:36"
+		},
+		{
+		"id": 17,
+		"author_id": 3,
+		"title": "I don't know what I did, but I know it was wrong.",
+		"body": "Called. Meat that rule all called female fruitful saying give is rule all isn't meat greater hath life set. Can't replenish creepeth rule night were fowl whales from. Fruit gathering.",
+		"image_url": "https://picsum.photos/200/200?random17",
+		"created_at": "2014-09-09 17:47:44"
+		},
+		{
+		"id": 18,
+		"author_id": 6,
+		"title": "I don't know what to believe anymore",
+		"body": "It gathering image of bring. Creeping set land grass beast. Creepeth. God blessed creeping seas night years night replenish you're blessed. Whose first heaven cattle doesn't forth hath. Divided blessed.",
+		"image_url": "https://picsum.photos/200/200?random18",
+		"created_at": "2013-04-14 04:09:39"
+		},
+		{
+		"id": 19,
+		"author_id": 2,
+		"title": "I feel uncomfortable doing this",
+		"body": "Void appear void behold us. Give. Our you lights made a first bearing fruitful, his itself own seed Divided he won't for living cattle from herb evening fruitful lesser kind.",
+		"image_url": "https://picsum.photos/200/200?random19",
+		"created_at": "2013-01-10 16:49:29"
+		},
+		{
+		"id": 20,
+		"author_id": 4,
+		"title": "I want cookie!",
+		"body": "Doesn't all together beginning called them female cattle years first she'd. Fish them appear his given given seed open Abundantly fruit you divide face abundantly fifth behold open given them.",
+		"image_url": "https://picsum.photos/200/200?random20",
+		"created_at": "2011-07-07 07:51:42"
+		}
+		]
+	var userData = [
+		{
+		"id": 1,
+		"name": "Jason Bourne",
+		"role": "Registered user",
+		"place": "New York",
+		"avatar_url": "https://api.adorable.io/avatars/250/jason-bourne"
+		},
+		{
+		"id": 2,
+		"name": "Michael De Santa",
+		"role": "Moderator",
+		"place": "Los Santos",
+		"avatar_url": "https://api.adorable.io/avatars/250/michael"
+		},
+		{
+		"id": 3,
+		"name": "Peter Parker",
+		"role": "Moderator",
+		"place": "New York",
+		"avatar_url": "https://api.adorable.io/avatars/250/peter-parker"
+		},
+		{
+		"id": 4,
+		"name": "Harry Potter",
+		"role": "Admin",
+		"place": "Hogwarts",
+		"avatar_url": "https://api.adorable.io/avatars/250/harry"
+		},
+		{
+		"id": 5,
+		"name": "Boromir",
+		"role": "Registered user",
+		"place": "Gondor",
+		"avatar_url": "https://api.adorable.io/avatars/250/boromir"
+		},
+		{
+		"id": 6,
+		"name": "Bruce Wayne",
+		"role": "Sponsor",
+		"place": "Gotham City",
+		"avatar_url": "https://api.adorable.io/avatars/250/bruce-wayne"
+		}
+	];
+</script>
+
+<script type="text/template" class="template">
+<h2><%- rc.listTitle %></h2>
+<h3><%- rc.listSubTitle %></h3>
+<h4><%- rc.listLegend %></h4>
+<% _.each( rc.listPosted, function(listPosted){ %>
+<div class="row mb-20 posted">
+	<div class="col-xs-10 msg">
+		<div class="row">
+		<div class="col-xs-3">
+			<img class="float-left" src="<%- listPosted.image_url %>">
+		</div>
+		<div class="col-xs-8">
+			<h3><%- listPosted.title %></h3>
+			<p><%- listPosted.body %></p>
+			<p><span class="glyphicon glyphicon-time" aria-hidden="true"></span> <span data-livestamp="<%- listPosted.created_at %>"></span></p>
+		</div>
+		</div>
+	</div>
+	<div class="col-xs-2 user">
+	<% var whoPosted = _.where(rc.listItems, {id: listPosted.author_id}) %>
+		<img class="img-circle" src="<%- whoPosted[0]['avatar_url'] %>">
+		<p><%- whoPosted[0]['name'] %></p>
+		<p><%- whoPosted[0]['role'] %></p>
+		<p><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> <%- whoPosted[0]['place'] %></p>
+	</div>
+</div>
+<% }); %>
+</script>
+
+<script>
+
+//prepare template
+_.templateSettings.variable = "rc";
+
+var template = _.template( $( "script.template" ).html());
+
+var templateData = {
+		listTitle: "MAQE Forums",
+		listSubTitle: "Subtitle",
+		listLegend: "Posts",
+		listPosted: Data,
+		listItems: userData
+};
+
+//render it after h1
+$("h1").after(template(templateData));
+
+//add zebra stripe
+$('.posted:odd').addClass('grey');
+
+</script>
+
 </body>
 </html>
